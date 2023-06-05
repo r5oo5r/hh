c3c920efa44c221d86eea640429312097a8e665f
r5oo5r <135654591+r5oo5r@users.noreply.github.com>
2023-06-05 21:39:36 +0300

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,7 @@
+basic.show_string("khalid")
+a=100
+b=200
+if a>b :
+    basic.show_string("a is greater than b")
+else :
+      basic.show_string("b is greater than or equal to a")
---
+++ main.ts
@@ -0,0 +1,9 @@
+basic.showString("khalid")
+let a = 100
+let b = 200
+if (a > b) {
+    basic.showString("a is greater than b")
+} else {
+    basic.showString("b is greater than or equal to a")
+}
+
